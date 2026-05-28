9cd5da1c9fa23a5902aeaf283d846f73ac78df81
JADAVDHRUVIT21 <dhruvit715@gmail.com>
2026-05-28 09:57:38 +0530

    register captain done

--- Backend/app.js
+++ Backend/app.js
@@ -5,6 +5,7 @@ const cors = require('cors');
 const app = express();
 const userRoutes = require('./routes/user.routes');
 const connectToDb = require('./db/db');
+const captainRoutes = require('./routes/captain.routes');
 
 connectToDB();
 
@@ -17,5 +18,6 @@ app.get('/', (req, res) => {
 });
 
 app.use('/users', userRoutes);
+app.use('/captains', captainRoutes);
 
 module.exports = app;
--- Backend/server.js
+++ Backend/server.js
@@ -4,16 +4,28 @@ const cookieParser = require('cookie-parser');
 require('dotenv').config();
 
 const connectToDb = require('./db/db');
+
 const userRoutes = require('./routes/user.routes');
+const captainRoutes = require('./routes/captain.routes');
 
 const app = express();
 
 connectToDb();
 
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 
+
+
 app.use('/users', userRoutes);
+app.use('/captain', captainRoutes);
+
+
+app.get('/', (req, res) => {
+    res.send('API is working');
+});
+
 
 const port = process.env.PORT || 4000;
 
